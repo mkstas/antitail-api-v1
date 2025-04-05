e32441973f035de80c4be6344abd1c9f4d3ab967
mkstas <makarov.stn@gmail.com>
2025-04-06 01:45:56 +0300

    feat: add exceptions in modules users, profiles

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,22 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Profile } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProfileDto } from './dto/create-profile.dto';
-import { Profile } from '@prisma/client';
 
 @Injectable()
 export class ProfilesService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(dto: CreateProfileDto): Promise<Profile> {
-    const profile = await this.prismaService.profile.create({
-      data: { ...dto },
-    });
-    return profile;
-  }
-
   async findOne(userId: number) {
     const profile = await this.prismaService.profile.findUnique({
       where: { userId },
+    });
+    if (!profile) throw new NotFoundException('Profile is not found');
+    return profile;
+  }
+
+  async create(dto: CreateProfileDto): Promise<Profile> {
+    let profile = await this.findOne(dto.userId);
+    if (profile) throw new BadRequestException('Profile is already exists');
+    profile = await this.prismaService.profile.create({
+      data: { ...dto },
     });
     return profile;
   }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -8,17 +8,20 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async create(dto: CreateUserDto): Promise<User> {
-    const passwordHash = await bcrypt.hash(dto.password, 10);
-    const user = await this.prismaService.user.create({
-      data: { email: dto.email, passwordHash },
-    });
-    return user;
-  }
-
   async findOne(email: string): Promise<User> {
     const user = await this.prismaService.user.findUnique({
       where: { email },
+    });
+    if (!user) throw new NotFoundException('User is not found');
+    return user;
+  }
+
+  async create(dto: CreateUserDto): Promise<User> {
+    let user = await this.findOne(dto.email);
+    if (user) throw new BadRequestException('Email is already taken');
+    const passwordHash = await bcrypt.hash(dto.password, 10);
+    user = await this.prismaService.user.create({
+      data: { email: dto.email, passwordHash },
     });
     return user;
   }
